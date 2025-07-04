4de53d1058e6b9191ef454dc8347119e1cf15f05
yashikasoni <yashika.evdt@gmail.com>
2025-07-04 08:13:05 +0000

    conversion of datatypes

---
+++ 01_basics/03.conversionoperation.js
@@ -0,0 +1,25 @@
+let score = true 
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof  valueInNumber );
+console.log( valueInNumber) ;
+
+
+// "33"=> 33
+// "33abcd"=> NaN (not an number)
+// "true"=> 1; "false=>0"
+
+
+let isloggedIn = 1
+
+
+let booleanIsLoggedIn = Boolean (isloggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1=> true ; 0 =>false
+// ""=> false
+// "yashika"=> true
