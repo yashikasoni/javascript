8a219d91f6cee10f99296a58137666cb62af4dd5
yashikasoni <yashika.evdt@gmail.com>
2025-07-04 07:29:20 +0000

    container confidg done

---
+++ 01_basics/01-variables.js
@@ -0,0 +1,12 @@
+const accountId = 2112
+let accountEmail ="yashika@google.com"
+var accountPassword ="12345"
+accoutnCity ="jaipur"
+let accountState;
+// accountId = 2 //not allowed 
+
+accountEmail= "ys@ys.com"
+accountPassword ="0506"
+accountCity = "delhi"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity , accountState])
---
+++ 01_basics/02-datatypes.js
@@ -0,0 +1,27 @@
+"Use strict";// treat all js as newer version
+
+// alert( 3+3 )// we are using node.js not browser 
+
+console.log( 3+3 )// code readablity should be high 
+
+console.log("yashika")
+
+
+
+let name ="yashika"
+let age =  19
+let isloggedIn = false
+
+// number=> 2 to power 53
+// string => ""
+// boolean => true/flase
+// null => stand alone value
+// undefined => 
+// symbol=> unique
+
+//  objest
+
+console.log(typeof null );// output= object
+console.log(typeof undefined );// undefined
+
+
