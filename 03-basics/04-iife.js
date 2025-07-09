fbd63126519b987ea9f3dc714dcc35254d16865a
yashikasoni <yashika.evdt@gmail.com>
2025-07-09 11:30:11 +0000

    iife

---
+++ 03-basics/04-iife.js
@@ -0,0 +1,12 @@
+// Immediately Invoked function Expressions (IIFE) 
+
+
+(function chai(){
+    console.log(`DB CONNECTED`);
+    
+})() ;
+
+((name)=>{
+    console.log(`DB CONNECTION TWO ${name}`); 
+}) ("yashika") 
+
